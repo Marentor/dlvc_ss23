a7d24da91c4e8d3a8866bc26d301609b3ecdc2bc
Marentor <martin.suchon@gmail.com>
2023-05-12 16:03:19 +0200

    Assignment 2 part1 working

--- dlvc/assignment2/optimizer_2d.py
+++ dlvc/assignment2/optimizer_2d.py
@@ -8,10 +8,12 @@ import cv2
 # A 2D vector. Used in Fn as an evaluation point.
 Vec2 = namedtuple('Vec2', ['x1', 'x2'])
 
+
 class AutogradFn(torch.autograd.Function):
     '''
     This class wraps a Fn instance to make it compatible with PyTorch optimizers
     '''
+
     @staticmethod
     def forward(ctx, fn, loc):
         ctx.fn = fn
@@ -36,6 +38,7 @@ def load_image(fpath: str) -> np.ndarray:
     img = cv2.imread(fpath, cv2.IMREAD_GRAYSCALE)
     return np.array(img, dtype=float) / 255.0
 
+
 class Fn:
     '''
     A 2D function evaluated on a grid.
@@ -55,10 +58,8 @@ class Fn:
         Use the result to visualize the progress of gradient descent.
         '''
 
-        cmap = cv2.applyColorMap(self.fn ,cv2.COLORMAP_JET)
-        cv2.imshow('image',cmap)
-
-
+        cmap = cv2.applyColorMap((self.fn * 255).astype(np.uint8), cv2.COLORMAP_TWILIGHT)
+        return cmap
 
     def __call__(self, loc: Vec2) -> float:
         '''
@@ -66,11 +67,9 @@ class Fn:
         Raises ValueError if loc is out of bounds.
         '''
 
-
         # You can simply round and map to integers. If so, make sure not to set eps and learning_rate too low
         # Alternatively, you can implement some form of interpolation (for example bilinear)
-
-
+        return self.fn[int(loc[0]), int(loc[1])]
 
     def grad(self, loc: Vec2) -> Vec2:
         '''
@@ -78,11 +77,19 @@ class Fn:
         Raises ValueError if loc is out of bounds of fn or if eps <= 0.
         '''
 
-        if not all(0 <= x < s for x, s in zip(loc, fn.shape)):
-            raise ValueError("Location is out of bounds of the function.")
+        # Check if loc is out of bounds
+        if loc[0] < 0 or loc[0] >= self.fn.shape[0] or loc[1] < 0 or loc[1] >= self.fn.shape[1]:
+            raise ValueError("Location is out of bounds.")
         if self.eps <= 0:
             raise ValueError("Epsilon must be positive.")
 
+            # Compute the gradient using central differences
+        dx = (self.__call__(Vec2(loc.x1 + self.eps, loc.x2)) - self.__call__(Vec2(loc.x1 - self.eps, loc.x2))) / (
+                2 * self.eps)
+        dy = (self.__call__(Vec2(loc.x1, loc.x2 + self.eps)) - self.__call__(Vec2(loc.x1, loc.x2 - self.eps))) / (
+                2 * self.eps)
+
+        return Vec2(dx, dy)
 
 
 if __name__ == '__main__':
@@ -107,6 +114,8 @@ if __name__ == '__main__':
     # PyTorch uses tensors which are very similar to numpy arrays but hold additional values such as gradients
     loc = torch.tensor([args.sx1, args.sx2], requires_grad=True)
     optimizer = torch.optim.SGD([loc], lr=args.learning_rate, momentum=args.beta, nesterov=args.nesterov)
+    losses = []
+    old_loc = loc
 
     # Find a minimum in fn using a PyTorch optimizer
     # See https://pytorch.org/docs/stable/optim.html for how to use optimizers
@@ -117,6 +126,24 @@ if __name__ == '__main__':
         # This returns the value of the function fn at location loc.
         # Since we are trying to find a minimum of the function this acts as a loss value.
         # loss = AutogradFn.apply(fn, loc)
+        optimizer.zero_grad()
+        loss = AutogradFn.apply(fn, loc)
+        loss.backward()
+        optimizer.step()
 
+        # if losses is not lower then 10 previous terminate
+        # if len(losses) > 10:
+        #     if loss > losses[-1]:
+        #         break
+        #     losses.pop(9)
+        #     losses.append(loss)
+        #     losses.sort()
+
+        if loss<0.1:
+            break
+        # Visualize the progress
+        cv2.line(vis, (int(old_loc[0].item()), int(old_loc[1].item())),(int(loc[0].item()), int(loc[1].item())), (0, 0, 255), 10)
         cv2.imshow('Progress', vis)
-        cv2.waitKey(50)  # 20 fps, tune according to your liking
+        cv2.waitKey(5)  # 20 fps, tune according to your liking
+        cv2.destroyAllWindows()
+        old_loc=loc
